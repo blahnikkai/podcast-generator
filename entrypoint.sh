bf8ef84fadcda019da096eeae32f77afec99b509
Kai Blahnik <83978042+blahnikkai@users.noreply.github.com>
2024-06-25 14:37:54 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,9 @@ git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
-git remote add origin https://github.com/Duhduh420/Duhduh420.git
 git add .
 git commit -m "Update feed"
-echo -e "blahnikkai\n${{ secrets.GH_ACCESS_TOKEN }}" | git push --set-upstream origin main
+echo git push -u origin main
 
 
 
